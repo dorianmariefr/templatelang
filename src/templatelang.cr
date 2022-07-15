e4dcd0bf6566fc50e3faedee6730cb8f2d5084e0
Dorian Marié <dorian@thoughtbot.com>
2022-07-15 16:18:46 +0200

    reply with plain text

--- src/templatelang.cr
+++ src/templatelang.cr
@@ -6,23 +6,27 @@ get "/" do
 end
 
 get "/api/template" do |env|
+  env.response.content_type = "text/plain"
   input = env.params.query.fetch("input", "")
   context = env.params.query.fetch("context", "")
   Template.render(input, context)
 end
 
 post "/api/template" do |env|
+  env.response.content_type = "text/plain"
   input = env.params.body.fetch("input", "")
   context = env.params.body.fetch("context", "")
   Template.render(input, context)
 end
 
 get "/api/code" do |env|
+  env.response.content_type = "text/plain"
   input = env.params.query.fetch("input", "")
   Code.evaluate(input).to_s
 end
 
 post "/api/code" do |env|
+  env.response.content_type = "text/plain"
   input = env.params.body.fetch("input", "")
   Code.evaluate(input).to_s
 end
